ab27a7baf68acfb83ba9da9e3ffd7d74a710e89a
Alfonso Braulio <Alfonso Braulio>
2016-11-08 21:25:34 +0100

    8/10/16

--- 2. zip_dictionary/zip_dictionary.R
+++ 2. zip_dictionary/zip_dictionary.R
@@ -12,12 +12,10 @@ work.path <- "C:/ALFONSO/WORK/GSE/14D003/"
 setwd(work.path)
 
 file.list <- as.data.frame(read.csv(paste0(work.path, "filenames.csv"), header = FALSE, ";", stringsAsFactors = F))
-
 zip.dict <- data.frame(ZIP_code= integer(0), City= character(0), State = character(0))
 
 # fore each csv file: read Zip, City, State --> extract unique Zips -->
 # --> clean the sub-code --> add to the dictionary
-
 for (f in 1:nrow(file.list)) {
   df <- as.data.frame(read.csv(paste0(work.path, "use/", file.list[f, 1]), header = T, ",", stringsAsFactors = F))
   z.c.s <- select(df, ZIP, CITY, STATE)
@@ -27,3 +25,5 @@ for (f in 1:nrow(file.list)) {
 
 # write the dictionary onto a csv file
 write.table(zip.dict, file = "zip_dict.csv", row.names = F, col.names = T)
+
+# EOC
